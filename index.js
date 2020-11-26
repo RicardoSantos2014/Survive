02ea7fdecd0103c82a34b600a66b51b265959f8c
Ricardo Santos <ricardo.c.r.santos@gmail.com>
2020-11-26 01:04:04 -0300

    COMMIT 26.11.20 - Remade layout

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ var diferencaDeAcerto = [];
 var chanceDeAcerto = [];
 
 $(document).ready(function(){
+    /**
+     * Initially, the page form must be cleared and gotten ready to data input.
+     */
+
     $('#nomePersonagem').val("");
     $('#ataquePersonagem').val("");
     $('#danoPersonagem').val("");
@@ -30,6 +34,11 @@ $(document).ready(function(){
     $("#btnAddPartyMember").click(() => {
         if ($('#nomePersonagem').val() != "" )
         {
+            /**
+            * When the "Add Party Member" button is clicked, the party data is stored in an array and readied to be shown in the page and afterwards
+            * the input fields are cleared.
+            * 
+            */
             personagem = {
                 nome: $('#nomePersonagem').val(),
                 ataque: $('#ataquePersonagem').val(),
@@ -39,11 +48,11 @@ $(document).ready(function(){
                 vida: $('#pontosDeVidaPersonagem').val(),
             };
             party.push(personagem);
-            $(`<p> <div class="divNome"> ${personagem.nome} </div>
-                    <div class="divAtaque"> ${personagem.ataque} </div>
-                    <div class="divDano"> ${personagem.dano} </div>
-                    <div class="divDefesa"> ${personagem.defesa} </div> 
-                    <div class="divVida"> ${personagem.vida} </div>  </p> <br/>`).appendTo('#personagensDaParty');
+            $(`<div class="divTableCell">${personagem.nome}</div>
+                <div class="divTableCell">${personagem.ataque}</div>
+                <div class="divTableCell">${personagem.dano}</div>
+                <div class="divTableCell">${personagem.defesa}</div>
+                <div class="divTableCell">${personagem.vida}</div>`).appendTo('#personagensDaParty');
             $('#nomePersonagem').val("");
             $('#ataquePersonagem').val("");
             $('#danoPersonagem').val("");
@@ -55,6 +64,12 @@ $(document).ready(function(){
     $("#botaoCalcular").click(() => {
         if ($('#nomeInimigo').val() != "" )
         {
+            /**
+             * When the "Calculate" button is clicked, the enemy data is loaded into an array and shown in the web page.
+             * 
+             * In a future update, it will be possible to add more than one enemy to simulate the battle. At this time, however, the 
+             * model will only supprt one enemy to make the mathematics easier.
+             */
             personagem = {
                 nome: $('#nomeInimigo').val(),
                 ataque: $('#ataqueInimigo').val(),
@@ -75,12 +90,25 @@ $(document).ready(function(){
             $('#defesaInimigo').val("");
             $('#pontosDeVidaInimigo').val("");
         }
+
+        // In this line we display the enemy name to create the battle report
         $(`<p> <div> <h1 class="tituloRelatorio"> Relatório de combate contra o ${inimigos[0].nome} </h1> </div> </p>`).appendTo('#relatorioDeCombate');
 
+        /**
+         * In the next function, we calculate the probability to hit the enemy for each party member. This information is stored in the array
+         * that contains the characters data.
+         */
         calcularDiferencaDeAcerto();
 
+        /**
+         * In the next function, the total damage of the party is calculated based on how much damage each character deals and their probability 
+         * to hit the enemy
+         */
         valorDanoTotalParty = danoTotalParty();
 
+        /**
+         * Now, for each character the statistical elements are calculated and the 
+         */
         party.forEach((persona, index) => {
             $(`<p> <div> O personagem ${persona.nome} e precisa de ${diferencaDeAcerto[index]} ou mais para acertar, 
                 resultando em acertos ${chanceDeAcerto[index]}% das vezes com dano médio de ${persona.danoMedio} por ataque bem-sucedido, o 
